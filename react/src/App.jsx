b87a22e150dc79092470eef6c42a568d28b1f84c
Assaduzzaman Shaon <assad.uz255@gmail.com>
2025-12-01 13:22:34 +0600

    Update

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -5,6 +5,8 @@ import NavBar from './components/NavBar';
 import List from './pages/customer/list'; 
 import Insert from './pages/customer/insert';
 import Edit from './pages/customer/edit';
+import Login from './pages/Auth/Login'; 
+import Register from './pages/Auth/Register';
 
 
 function App() {
@@ -16,6 +18,8 @@ function App() {
                     <Route path="/" element={<List />} /> 
                     <Route path="/insert" element={<Insert />} />
                     <Route path="/edit/:id" element={<Edit />} />
+                    <Route path="/login" element={<Login />} /> 
+                    <Route path="/register" element={<Register />} />
                 </Routes>
             </div>
         </Router>
--- react/src/components/NavBar.jsx
+++ react/src/components/NavBar.jsx
@@ -6,14 +6,35 @@ const NavBar = () => {
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container">
                 <NavLink className="navbar-brand" to="/">Customer CRUD</NavLink>
-                <div className="collapse navbar-collapse">
+                
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    {/* বাম দিকে: Customer Management Links */}
                     <ul className="navbar-nav me-auto">
                         <li className="nav-item">
-                            {/* '/' Path টি list.jsx কে লোড করবে */}
                             <NavLink className="nav-link" to="/">Customer List</NavLink>
                         </li>
                         <li className="nav-item">
                             <NavLink className="nav-link" to="/insert">Add Customer</NavLink>
+                        </li>
+                    </ul>
+                    
+                    {/* ✅ ডান দিকে: Auth Links */}
+                    <ul className="navbar-nav ms-auto"> 
+                        <li className="nav-item">
+                            <NavLink 
+                                className="nav-link btn btn-outline-light me-2" // স্টাইল যোগ করা হলো
+                                to="/login"
+                            >
+                                Login
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink 
+                                className="nav-link btn btn-success" // স্টাইল যোগ করা হলো
+                                to="/register"
+                            >
+                                Register
+                            </NavLink>
                         </li>
                     </ul>
                 </div>
